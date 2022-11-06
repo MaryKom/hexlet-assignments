87bdead55102e7b805bacbc7e258833803951f3c
Maria Komendantova <masha.komendantova@yandex.ru>
2022-11-06 14:48:57 +0300

    add homwerk #1

--- java-collections-ru/lists/src/main/java/exercise/App.java
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -2,7 +2,23 @@ package exercise;
 
 import java.util.Arrays;
 import java.util.ArrayList;
+import java.util.List;
 
 // BEGIN
+class App {
+    public static boolean scrabble(String symbols, String word) {
+        String[] arraySymbol = symbols.toLowerCase().split("");
+        String[] arrayWord = word.toLowerCase().split("");
+        List<String> listSymbol = new ArrayList<>(Arrays.asList(arraySymbol));
 
+        for (String s: arrayWord) {
+            if (listSymbol.toString().contains(s)) {
+                listSymbol.remove(s);
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
 //END
